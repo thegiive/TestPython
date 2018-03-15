18fd50e41fa4a5749b0e3069d2ac98539830ef33
Wisely Chen <kai-hsiung.chen@ge.com>
2018-03-15 20:45:59 +0800

    Solve one task

---
+++ checkPalindrome.py
@@ -0,0 +1,7 @@
+def checkPalindrome(inputString):
+    #https://codefights.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ
+    if(inputString[::-1] == inputString):
+        return True 
+    else:
+        return False
+    
