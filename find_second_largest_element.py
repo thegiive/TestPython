90dc8a63c761f255f0423b0d3294afdd86b59b82
Wisely Chen <kai-hsiung.chen@ge.com>
2018-03-16 12:09:40 +0800

    Find second largest element

---
+++ find_second_largest_element.py
@@ -0,0 +1,13 @@
+def find_second_largest_element(element_list):
+    max = float('-inf')
+    second_max = float('-inf')
+    for ele in element_list:
+        if(ele > max):
+            second_max = max 
+            max = ele
+        elif(ele > second_max):
+            second_max = ele
+    return second_max
+
+input_list = [3,1,2,5,4,6]
+print(find_second_largest_element(input_list))
