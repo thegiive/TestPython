4d44bc73cfe46d29668b4dd4d746eefa7082c81b
Wisely Chen <thegiive@gmail.com>
2018-03-16 12:23:13 +0800

    Add comment

--- find_second_largest_element.py
+++ find_second_largest_element.py
@@ -1,9 +1,10 @@
+# https://codefights.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m
 def find_second_largest_element(element_list):
     max = float('-inf')
     second_max = float('-inf')
     for ele in element_list:
         if(ele > max):
-            second_max = max 
+            second_max = max
             max = ele
         elif(ele > second_max):
             second_max = ele
