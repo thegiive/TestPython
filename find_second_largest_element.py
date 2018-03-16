0e6c47a375e200152175c735f90b4a129108aebb
Wisely Chen <thegiive@gmail.com>
2018-03-16 17:46:59 +0800

    Solve problem

--- find_second_largest_element.py
+++ find_second_largest_element.py
@@ -10,5 +10,35 @@ def find_second_largest_element(element_list):
             second_max = ele
     return second_max
 
-input_list = [3,1,2,5,4,6]
-print(find_second_largest_element(input_list))
+
+
+# def adjacentElementsProduct(inputArray):
+#     max1 = max(inputArray)
+#     inputArray.remove(max1)
+#     max2 = max(inputArray)
+#     inputArray.append(max1)
+#     min1 = min(inputArray)
+#     inputArray.remove(min1)
+#     min2 = min(inputArray)
+#     print(max1)
+#     print(max2)
+#     print(min1)
+#     print(min2)
+#     if(max1 * max2 > min1 *min2):
+#         return max1*max2 
+#     else:
+#         return min1*min2
+
+
+def adjacentElementsProduct(inputArray):
+    max = float('-inf')
+    for i in range(0,(len(inputArray)-1) ):
+        if( inputArray[i] * inputArray[i+1] > max):
+            max = inputArray[i] * inputArray[i+1]
+    return max
+
+# input_list = [3,1,2,5,4,6 , -10 , -20 ]
+input_list = [3, 6, -2, -5, 7, 3]
+print(adjacentElementsProduct(input_list))
+# print(find_second_largest_element(input_list))
+# print(adjacentElementsProduct(input_list))
