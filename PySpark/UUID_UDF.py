3ff0d522049314cda56db076eda4636787e9f6e5
Wisely Chen <thegiive@gmail.com>
2018-03-20 14:35:15 +0800

    Add pyspark udf register example

---
+++ PySpark/UUID_UDF.py
@@ -0,0 +1,11 @@
+rom uuid import UUID
+from pyspark.sql.types import *
+
+def getUUID():
+        return uuid.uuid4().bytes
+
+# print(getUUID())
+
+spark.udf.register("UUID" ,  getUUID , BinaryType() )
+spark.sql("select UUID() as id ").write.format("jdbc").option("url", "jdbc:postgresql://localhost/postgres?user=xxx&password=xxx")\
+                 .option("dbtable", "shipment_statistics").option("user" , "xxx").option("password", "xxx").mode("append").save()
