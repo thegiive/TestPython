0550f978a1c58b6f3106c92acdd70630aa004827
Wisely Chen <thegiive@gmail.com>
2018-03-20 14:36:14 +0800

    Fix table name

--- PySpark/UUID_UDF.py
+++ PySpark/UUID_UDF.py
@@ -8,4 +8,4 @@ def getUUID():
 
 spark.udf.register("UUID" ,  getUUID , BinaryType() )
 spark.sql("select UUID() as id ").write.format("jdbc").option("url", "jdbc:postgresql://localhost/postgres?user=xxx&password=xxx")\
-                 .option("dbtable", "shipment_statistics").option("user" , "xxx").option("password", "xxx").mode("append").save()
+                 .option("dbtable", "xxxx").option("user" , "xxx").option("password", "xxx").mode("append").save()
